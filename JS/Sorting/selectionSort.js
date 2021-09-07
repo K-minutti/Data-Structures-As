ca7f1fe94d07aaaa063e7ee6f201217d585f34c9
Kevin Minutti <minutt70@gmail.com>
2021-09-07 13:30:24 -0400

    Setup file for selection sort

---
+++ JS/Sorting/selectionSort.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} array
+ * @return {number[]} 
+ */
+
+
+const selectionSort = (array) => {
+    return array;
+}
+
+
+console.log(insertionSort([34,8,7,9,6,5,3,69,4,22,]));
+console.log(insertionSort([1, -2, -5,5, 80, 10, 34, 3,22,]));
+console.log(insertionSort([9, 4,13,8,91,2,4,6,50,3,5,61,67,7]));
