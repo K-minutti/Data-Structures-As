d5533463f2dd36e233b980d5f9dd4c2ad12966c3
Kevin Minutti <minutti70@gmail.com>
2021-09-07 13:42:10 -0400

    Setup file for binary search

---
+++ JS/Searching/binarySearch.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} array
+ * @return {boolean}} 
+ */
+
+ const binarySearch = (array) => {
+    return false;
+}
+
+console.log([1,2,3,4,5,6,7,8,9]);
+console.log([1,2,7,14,15,20,27,30,49]);
+console.log([5,12,15,24,34,36,47,58,69]);
