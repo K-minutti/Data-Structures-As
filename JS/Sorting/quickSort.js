0e0909fdfca641538894f35b7c0e5a87712595ed
Kevin Minutti <minutti70@gmail.com>
2021-09-07 18:09:04 -0400

    Setup quick sort file and cleaned up files.

---
+++ JS/Sorting/quickSort.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} array
+ * @return {number[]} 
+ */
+
+
+ const quickSort = (array) => {
+     // quicksort = pivot
+     // recursive algo
+     return array;
+}
+
+const getPivot = (array, start=0, end=array.length-1)=> {
+
+}
+
+console.log(quickSort([34,8,7,9,6,5,3,69,4,22,]));
+console.log(quickSort([1, -2, -5,5, 80, 10, 34, 3,22,]));
+console.log(quickSort([9, 4,13,8,91,2,4,6,50,3,5,61,67,7]));
--- JS/Sorting/selectionSort.js
+++ JS/Sorting/selectionSort.js
@@ -9,6 +9,6 @@ const selectionSort = (array) => {
 }
 
 
-console.log(insertionSort([34,8,7,9,6,5,3,69,4,22,]));
-console.log(insertionSort([1, -2, -5,5, 80, 10, 34, 3,22,]));
-console.log(insertionSort([9, 4,13,8,91,2,4,6,50,3,5,61,67,7]));
+console.log(selectionSort([34,8,7,9,6,5,3,69,4,22,]));
+console.log(selectionSort([1, -2, -5,5, 80, 10, 34, 3,22,]));
+console.log(selectionSort([9, 4,13,8,91,2,4,6,50,3,5,61,67,7]));
