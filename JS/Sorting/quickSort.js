048952bf58ec145b7e40303ee8cbd71774f4af80
Kevin Minutti <minutti70@gmail.com>
2021-09-07 22:16:13 -0400

    Add quicksort algo

--- JS/Sorting/quickSort.js
+++ JS/Sorting/quickSort.js
@@ -3,15 +3,36 @@
  * @return {number[]} 
  */
 
-
- const quickSort = (array) => {
-     // quicksort = pivot
-     // recursive algo
+const quickSort = (array) => {
+     quickSortRecursiveFunc(array, 0, array.length-1);
      return array;
 }
 
-const getPivot = (array, start=0, end=array.length-1)=> {
+const quickSortRecursiveFunc = (array, left, right) => {
+    if(left < right){
+        const pivotRestingPositiion = getPivot(array, left, right);
+        quickSortRecursiveFunc(array, left, pivotRestingPositiion-1);
+        quickSortRecursiveFunc(array, pivotRestingPositiion+1, right);
+    }
+}
 
+const getPivot = (array, left=0, right=array.length-1)=> {
+    const pivotElem = array[right];
+
+    let p2 = left-1
+    for(let p1= left; p1 <right; ++p1){
+        if(array[p1] <= pivotElem){
+            p2++;
+            swap(array, p2, p1);
+        }
+    }
+
+    swap(array, p2+1, right);
+    return p2+1;
+}
+
+const swap = (array, idx1, idx2) => {
+    [array[idx1], array[idx2]] = [array[idx2], array[idx1]]
 }
 
 console.log(quickSort([34,8,7,9,6,5,3,69,4,22,]));
