dc96a7755e4efe587e583ec683d72a441d08e5eb
Kevin Minutti <minutti70@gmail.com>
2021-09-15 14:10:44 -0400

    DDL setup

---
+++ JS/DS/LinkedLists/doublyLinkedList.js
@@ -0,0 +1,22 @@
+/** 
+ * class obj
+ * 
+ * 
+*/
+
+class Node {
+    constructor(value){
+        this.val = value;
+        this.next = null;
+        this.prev = null;
+    }
+
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
