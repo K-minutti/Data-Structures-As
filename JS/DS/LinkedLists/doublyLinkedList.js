3db13193ade75d86b257629b8a81094da06910c2
Kevin Minutti <minutti70@gmail.com>
2021-09-18 15:28:57 -0400

    Create get and set methods on DLL

--- JS/DS/LinkedLists/doublyLinkedList.js
+++ JS/DS/LinkedLists/doublyLinkedList.js
@@ -82,6 +82,36 @@ class DoublyLinkedList {
         return this;
     }
 
+    get(index){
+        if (index < 0 || index >= this.length) return null;
+        let count, current;
+        if(index <= this.length/2){
+            count = 0;
+            current = this.head;
+            while (count !== index){
+                current = current.next;
+                count+=1;
+            }
+        } else {
+            count = this.length-1;
+            current = this.tail;
+            while(count !== index){
+                current = current.prev;
+                count-=1;
+            }
+        }
+        return current;
+    }
+
+    set(index, value){
+        let node =  this.get(index);
+        if(node !== null){
+            node.value = value;
+            return true;
+        }
+        return false;
+    }
+
 }
 
 const list = new DoublyLinkedList();
