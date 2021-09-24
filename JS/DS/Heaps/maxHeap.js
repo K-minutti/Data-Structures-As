94582b217eb4b53bce73d389086d6014588d1c11
Kevin Minutti <minutti70@gmail.com>
2021-09-24 14:12:22 -0400

    Add extract method to maxHeap

--- JS/DS/Heaps/maxHeap.js
+++ JS/DS/Heaps/maxHeap.js
@@ -21,6 +21,24 @@ class MaxBinaryHeap {
         }   
     }
 
+    extractMax(){
+        if(this.heap.length === 0)return undefined;
+        let maxElement = this.heap[0];
+        let lastElement = this.heap.pop();
+        this.heap[0] = lastElement;
+        // while the root is not in its correct place bubble down
+        let currIdx = 0; //root
+        let heapLen = this.heap.length;
+        let element = this.heap[0];
+        while(true){
+        // swap the root with child nodes
+            break;
+        }
+
+        //
+        return maxElement;
+    }
+
     print(){
         console.log(this.heap);
     }
