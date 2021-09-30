d1496906a06d268601a530754f446e3cf1d87dec
Kevin Minutti <minutti70@gmail.com>
2021-09-30 14:30:22 -0400

    Create method to add vertex in graph

--- JS/DS/Graphs/adjacencyList.js
+++ JS/DS/Graphs/adjacencyList.js
@@ -1,9 +1,17 @@
 /**
  * Basic graph using adjacency list using a map
+ * undirected
 */
 
 class Graph {
     constructor(){
         this.adjacencyList = {};
+    }
+
+    addVertex(vertex){
+        if(!this.adjacencyList){
+            this.adjacencyList[vertex] = [];
+        }
+        // if already exists do nothing
     }
 }
