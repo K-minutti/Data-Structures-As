275acc309224d362c86c0c9246b512eafad9ceeb
Kevin Minutti <minutti70@gmail.com>
2021-10-05 21:42:35 -0400

    Add DFS and BFS to graph

--- JS/DS/Graphs/adjacencyList.js
+++ JS/DS/Graphs/adjacencyList.js
@@ -41,14 +41,15 @@ class Graph {
     DFSRecursive(startNode){
         const res = []; // list to return search output 
         const visited = {}; // map of visited nodes/vertex
+        const adjList = this.adjacencyList;
 
         function depthFirstSearch(vertex){
             if(!vertex) return null;
             visited[vertex] = true;
             res.push(visited);
-            let nodeLen = this.adjacencyList[vertex].length;
+            let nodeLen = adjList[vertex].length;
             for(let i = 0; i < nodeLen; ++i){
-                let adjNode = this.adjacencyList[vertex][i];
+                let adjNode = adjList[vertex][i];
                 if(!visited[adjNode]){
                     depthFirstSearch(adjNode)
                 }
@@ -60,7 +61,49 @@ class Graph {
     }
 
 
-    DFSIterative(start){
-        return null;
+    DFSIterative(startNode){
+        let res = [];
+        let visited = {};
+        let stack = [startNode];// add starting node to graph
+        let adjList = this.adjacencyList;
+        let currNode; // init var outside of while loop
+        visited[startNode] = true; //marking starting node as seen
+
+        while(stack.length){
+            currNode = stack.pop();
+            res.push(currNode);// add node to search results 
+            // adding nodes with edges to currNode
+            for(let i = 0; i <adjList[currNode].length;++i){
+                let adjNode = adjList[currNode][i];
+                if(!visited[adjNode]){
+                    visited[adjNode] =true;
+                    stack.push(adjNode);
+                }
+            }
+        }
+        return res;
+    }
+
+    BreadthFirstSearch(startNode){
+        let res = [];
+        let queue = [startNode];
+        let visited = {};
+        let adjList = this.adjacencyList;
+        let currNode;
+        visited[startNode] = true;
+        while(queue.length){
+            currNode = queue.shift();
+            res.push(node);
+
+            adjList[currNode].forEach(adjNode => {
+                if(!visited[adjNode]){
+                    visited[adjNode] = true; // setting as seen
+                    queue.push(adjNode) // now that we have seen the node 
+                    // we also have to visit its neighbors which we will get to once
+                    // we all foreach on the node.
+                }
+            })
+        }
+        return res;
     }
 }
