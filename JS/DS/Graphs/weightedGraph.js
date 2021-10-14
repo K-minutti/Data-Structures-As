3aedb68106ffb78413e5879a257735c8521a7c18
Kevin Minutti <minutti70@gmail.com>
2021-10-13 21:06:40 -0400

    Dijkstra's algorithm

--- JS/DS/Graphs/weightedGraph.js
+++ JS/DS/Graphs/weightedGraph.js
@@ -39,13 +39,13 @@ class WeightedGraph {
         this.adjacencyList[vertexTwo].push({vertex: vertexOne, weight})
     }
 
-    shortesPathDijkstra(start, finish) {
-        // setup algo
+    shortesPathDijkstra(start, finish) {// returns path as list of nodes in order
+        // setup algo 
         const nodes = new PriorityQueue();
         const distances = {};
         const previous = {};
-
-        // let smallest;
+        let smallest;
+        let path = [];
 
         // creating initial state for algorithm all nodes will be set to a len of 
         // infinity excep for the current node-start
@@ -61,8 +61,33 @@ class WeightedGraph {
         }
         // logic 
         while(nodes.values.length){
-            nodes.dequeue();
+            smallest = nodes.dequeue().value; // minHeap priorityQueue
+            if(smallest === finish){
+                while(previous[smallest]){
+                    path.push(smallest);
+                    smallest = previous[smallest];
+                }
+                break;
+            }
+
+            if(smallest || distances[smallest] !== Infinity){
+                for(let neighbor in this.adjacencyList[smallest]){
+                    // get next node
+                    let nextNode = this.adjacencyList[smallest][neighbor];
+                    // calc new distance to next node
+                    let potentialNextNode = distances[smallest] + nextNode.weight;
+                    let nextNeighbor = nextNode.node;
+                    if(potentialNextNode < distances[nextNeighbor]){
+                        // updating new smallest distance to neighbor
+                        distances[nextNeighbor] = potentialNextNode;
+                        // updating previous - How we got to Neighbor
+                        previous[nextNeighbor] = smallest;
+                        // enqueue in priority queue with new priority
+                        nodes.enqueue(nextNeighbor, potentialNextNode);
+                    }
+                }
+            }
         }
-        return null
+        return path.concat(smallest).reverse();
     }
 }
